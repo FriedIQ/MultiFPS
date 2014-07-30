78304384d1fb7115b60ba427082ddc73b1d9e388
FriedIQ <fried.iq@gmail.com>
2014-07-30 13:43:07 -0400

    Implimented Teams
    Crude Teams with selection at start and respawn. Basic teamplay rules
    implimented.

--- MultiFPS/Assets/Scripts/NetworkManager.cs
+++ MultiFPS/Assets/Scripts/NetworkManager.cs
@@ -1,73 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(PhotonView))]
-public class NetworkManager : MonoBehaviour {
-
-	public bool OfflineMode = false;
-	public GameObject MenuCamera;
+[RequireComponent(typeof (PhotonView))]
+public class NetworkManager : MonoBehaviour
+{
+    public bool OfflineMode = false;
+    public GameObject MenuCamera;
     public float RespawnTimer = 0;
 
-    private PhotonView photonView;
-    private List<string> chatMessages;
-    private int chatBufferSize = 5;
+    private PhotonView _photonView;
+    private List<string> _chatMessages;
+    private const int ChatBufferSize = 5;
+    private bool _teamChosen;
 
-	// Use this for initialization
-	void Start () {
-        photonView = GetComponent<PhotonView>();
-        PhotonNetwork.player.name = PlayerPrefs.GetString( "PlayerName", "Unknown Player" );
-        chatMessages = new List<string>();
-	}
-
-    void Update()
+    // Use this for initialization
+    private void Start()
     {
-        if (RespawnTimer > 0)
-        {
-            RespawnTimer -= Time.deltaTime;
-
-            if (RespawnTimer <= 0)
-            {
-                SpawnPlayer();
-                RespawnTimer = 0;
-            }
-        }
+        _photonView = GetComponent<PhotonView>();
+        PhotonNetwork.player.name = PlayerPrefs.GetString("PlayerName", "Unknown Player");
+        _chatMessages = new List<string>();
     }
 
-	void Connect()
-	{
-		PhotonNetwork.ConnectUsingSettings( "0.0.1" );
-	}
-
-    void OnDestroy()
+    private void Update()
     {
-        PlayerPrefs.SetString( "PlayerName", PhotonNetwork.player.name );
+        if (!(RespawnTimer > 0)) return;
+        RespawnTimer -= Time.deltaTime;
+
+        if (!(RespawnTimer <= 0)) return;
+        _teamChosen = false;
+        RespawnTimer = 0;
     }
 
-	void OnJoinedLobby()
-	{
-		//Debug.Log ( "OnJoinedLobby" );
-		PhotonNetwork.JoinRandomRoom(); 
-	}
+    private void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.0.1");
+    }
 
-	void OnPhotonRandomJoinFailed()
-	{
-		//Debug.Log ( "OnPhotonRandonJoinFailed" );
-		PhotonNetwork.CreateRoom( null );
-	}
+    private void OnDestroy()
+    {
+        PlayerPrefs.SetString("PlayerName", PhotonNetwork.player.name);
+    }
 
-	void OnJoinedRoom()
-	{
-		//Debug.Log ( "OnJoinedRoom" );
+    private void OnJoinedLobby()
+    {
+        //Debug.Log ( "OnJoinedLobby" );
+        PhotonNetwork.JoinRandomRoom();
+    }
 
-		SpawnPlayer();
-	}
+    private void OnPhotonRandomJoinFailed()
+    {
+        //Debug.Log ( "OnPhotonRandonJoinFailed" );
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    //private void OnJoinedRoom()
+    //{
+    //    Debug.Log ( "OnJoinedRoom" );
+    //}
 
     public void AddChatMessage(string text)
     {
-        if (photonView != null)
+        if (_photonView != null)
         {
-            photonView.RPC("AddChatMessageRpc", PhotonTargets.AllBuffered, text);
+            _photonView.RPC("AddChatMessageRpc", PhotonTargets.AllBuffered, text);
         }
         else
         {
@@ -76,35 +71,71 @@ public class NetworkManager : MonoBehaviour {
     }
 
     [RPC]
-    void AddChatMessageRpc(string text)
+    private void AddChatMessageRpc(string text)
     {
-        if (chatMessages.Count >= chatBufferSize)
+        if (_chatMessages.Count >= ChatBufferSize)
         {
-            chatMessages.RemoveAt(0);
+            _chatMessages.RemoveAt(0);
         }
-        chatMessages.Add(text);
+        _chatMessages.Add(text);
     }
 
-	void SpawnPlayer()
-	{
-		var spawnPoints = FindObjectsOfType<SpawnPoint>();
+    private void SpawnPlayer(int teamId)
+    {
+        _teamChosen = true;
+        var spawnPoints = FindObjectsOfType<SpawnPoint>();
 
-	    var point = UnityEngine.Random.Range(1, spawnPoints.Length);
+        var point = Random.Range(1, spawnPoints.Length);
         MenuCamera.SetActive(false);
-        PhotonNetwork.Instantiate("First Person Controller", spawnPoints[point].transform.position, spawnPoints[point].transform.rotation, 0);
-        AddChatMessage("Player " + PhotonNetwork.player.name + " has joined the game.");
-	}
 
-	void OnGUI()
-	{
+        var player = PhotonNetwork.Instantiate("First Person Controller", spawnPoints[point].transform.position,
+            spawnPoints[point].transform.rotation, 0);
+        SetTeam(player, teamId);
+
+        AddChatMessage("Player " + PhotonNetwork.player.name + " has joined the game.");
+    }
+
+    private static void SetTeam(GameObject player, int teamId)
+    {
+        if (player == null)
+        {
+            Debug.Log("Player is null");
+        }
+
+        player.GetComponentInChildren<PlayerTeam>().TeamId = teamId;
+        var skin = player.transform.GetComponentInChildren<SkinnedMeshRenderer>();
+
+        if (skin == null)
+        {
+            Debug.Log("SkinnedMeshRenderer not found");
+            return;
+        }
+
+        switch(teamId)
+        {
+        	case 1:
+                skin.material.color = Color.blue;
+                break;
+            case 2:
+                skin.material.color = Color.red;
+                break;
+            default:
+                skin.material.color = Color.white;
+                break;
+        }
+    }
+
+    private void OnGUI()
+    {
         GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
         GUILayout.BeginVertical();
-		GUILayout.Label( PhotonNetwork.connectionState.ToString() );
+        GUILayout.Label(PhotonNetwork.connectionState.ToString());
         GUILayout.Label("IsMasterClient: " + PhotonNetwork.isMasterClient.ToString());
         GUILayout.FlexibleSpace();
         GUILayout.EndVertical();
         GUILayout.EndArea();
 
+        // We are NOT connected nor are we currently ATTEMPTING to connect.
         if (!PhotonNetwork.connected && !PhotonNetwork.connecting)
         {
             GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
@@ -115,7 +146,7 @@ public class NetworkManager : MonoBehaviour {
 
             GUILayout.BeginHorizontal();
             GUILayout.Label("Name:");
-            PhotonNetwork.player.name = GUILayout.TextField( PhotonNetwork.player.name );
+            PhotonNetwork.player.name = GUILayout.TextField(PhotonNetwork.player.name);
             GUILayout.EndHorizontal();
 
             if (GUILayout.Button("Single Player"))
@@ -123,7 +154,7 @@ public class NetworkManager : MonoBehaviour {
                 PhotonNetwork.offlineMode = true;
                 OnJoinedLobby();
             }
-            if( GUILayout.Button( "Multi Player" ) )
+            if (GUILayout.Button("Multi Player"))
             {
                 Connect();
             }
@@ -134,19 +165,58 @@ public class NetworkManager : MonoBehaviour {
             GUILayout.EndArea();
         }
 
+        // We are FULLY connected.
         if (PhotonNetwork.connected && !PhotonNetwork.connecting)
         {
-            GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
-            GUILayout.BeginVertical();
-            GUILayout.FlexibleSpace();
-
-            foreach (var m in chatMessages)
+            if (_teamChosen)
             {
-                GUILayout.Label(m);
-            }
+                GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+                GUILayout.BeginVertical();
+                GUILayout.FlexibleSpace();
 
-            GUILayout.EndVertical();
-            GUILayout.EndArea();
+                foreach (var m in _chatMessages)
+                {
+                    GUILayout.Label(m);
+                }
+
+                GUILayout.EndVertical();
+                GUILayout.EndArea();
+            }
+            else
+            {
+                GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                GUILayout.BeginVertical();
+                GUILayout.FlexibleSpace();
+
+                GUILayout.Label("Select a Team:");
+                if (GUILayout.Button("Blue"))
+                {
+                    SpawnPlayer(1);
+                }
+                if (GUILayout.Button("Red"))
+                {
+                    SpawnPlayer(2);
+                }
+
+                if (GUILayout.Button("Random"))
+                {
+                    var teamId = Random.Range(0, 2);
+                    SpawnPlayer(teamId);
+
+                    if (GUILayout.Button("Renegade"))
+                    {
+                        SpawnPlayer(0);
+                    }
+                }
+
+                GUILayout.FlexibleSpace();
+                GUILayout.EndVertical();
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+                GUILayout.EndArea();
+            }
         }
-	}
+    }
 }
---
+++ MultiFPS/Assets/Scripts/PlayerTeam.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTeam : MonoBehaviour {
+	public int TeamId = 0;
+}
